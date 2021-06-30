dc5ac026d683b1d3953a34cce73b20c82f3d5c4e
Arjun Devoloper <mail.arjunks@gmail.com>
2021-06-30 11:34:13 +0500

    New BG Animation updated

--- script.js
+++ script.js
@@ -1,377 +1,146 @@
-const STAR_COUNT = (window.innerWidth + window.innerHeight) / 8,
-STAR_SIZE = 3,
-STAR_MIN_SCALE = 0.2,
-OVERFLOW_THRESHOLD = 50;
+jQuery(document).ready(function ($) {
 
-const canvas = document.querySelector('canvas'),
-context = canvas.getContext('2d');
-
-let scale = 1, // device pixel ratio
-width,
-height;
-
-let stars = [];
-
-let pointerX,
-pointerY;
-
-let velocity = { x: 0, y: 0, tx: 0, ty: 0, z: 0.0005 };
-
-let touchInput = false;
-
-generate();
-resize();
-step();
-
-window.onresize = resize;
-canvas.onmousemove = onMouseMove;
-canvas.ontouchmove = onTouchMove;
-canvas.ontouchend = onMouseLeave;
-document.onmouseleave = onMouseLeave;
-
-function generate() {
-
-  for (let i = 0; i < STAR_COUNT; i++) {
-    stars.push({
-      x: 0,
-      y: 0,
-      z: STAR_MIN_SCALE + Math.random() * (1 - STAR_MIN_SCALE) });
-
-  }
-
-}
-
-function placeStar(star) {
-
-  star.x = Math.random() * width;
-  star.y = Math.random() * height;
-
-}
-
-function recycleStar(star) {
-
-  let direction = 'z';
-
-  let vx = Math.abs(velocity.x),
-  vy = Math.abs(velocity.y);
-
-  if (vx > 1 || vy > 1) {
-    let axis;
-
-    if (vx > vy) {
-      axis = Math.random() < vx / (vx + vy) ? 'h' : 'v';
-    } else
-    {
-      axis = Math.random() < vy / (vx + vy) ? 'v' : 'h';
-    }
-
-    if (axis === 'h') {
-      direction = velocity.x > 0 ? 'l' : 'r';
-    } else
-    {
-      direction = velocity.y > 0 ? 't' : 'b';
-    }
-  }
-
-  star.z = STAR_MIN_SCALE + Math.random() * (1 - STAR_MIN_SCALE);
-
-  if (direction === 'z') {
-    star.z = 0.1;
-    star.x = Math.random() * width;
-    star.y = Math.random() * height;
-  } else
-  if (direction === 'l') {
-    star.x = -OVERFLOW_THRESHOLD;
-    star.y = height * Math.random();
-  } else
-  if (direction === 'r') {
-    star.x = width + OVERFLOW_THRESHOLD;
-    star.y = height * Math.random();
-  } else
-  if (direction === 't') {
-    star.x = width * Math.random();
-    star.y = -OVERFLOW_THRESHOLD;
-  } else
-  if (direction === 'b') {
-    star.x = width * Math.random();
-    star.y = height + OVERFLOW_THRESHOLD;
-  }
-
-}
-
-function resize() {
-
-  scale = window.devicePixelRatio || 1;
-
-  width = window.innerWidth * scale;
-  height = window.innerHeight * scale;
-
-  canvas.width = width;
-  canvas.height = height;
-
-  stars.forEach(placeStar);
-
-}
-
-function step() {
-
-  context.clearRect(0, 0, width, height);
-
-  update();
-  render();
-
-  requestAnimationFrame(step);
-
-}
-
-function update() {
-
-  velocity.tx *= 0.96;
-  velocity.ty *= 0.96;
-
-  velocity.x += (velocity.tx - velocity.x) * 0.8;
-  velocity.y += (velocity.ty - velocity.y) * 0.8;
-
-  stars.forEach(star => {
-
-    star.x += velocity.x * star.z;
-    star.y += velocity.y * star.z;
-
-    star.x += (star.x - width / 2) * velocity.z * star.z;
-    star.y += (star.y - height / 2) * velocity.z * star.z;
-    star.z += velocity.z;
-
-    // recycle when out of bounds
-    if (star.x < -OVERFLOW_THRESHOLD || star.x > width + OVERFLOW_THRESHOLD || star.y < -OVERFLOW_THRESHOLD || star.y > height + OVERFLOW_THRESHOLD) {
-      recycleStar(star);
-    }
-
+  // noise grain toggle
+  $('a.noiseTest').on('click', function (event) {
+      event.preventDefault();
+      $('.noise').toggleClass('active');
+      $(this).toggleClass('active');
   });
 
+});
+
+/* Credit to Collin Henderson @ AstralApp.com */
+
+(function() {
+var WIDTH, HEIGHT, canvas, con, g;
+var pxs = [];
+var rint = 50;
+
+$.fn.sprites = function () {
+this.append($('<canvas id="sprites"></canvas>'));
+setup(this);
 }
 
-function render() {
+function setup (container) {
+var windowSize = function() {
+WIDTH = container.innerWidth();
+HEIGHT = container.innerHeight();
+canvas = container.find('#sprites');
+canvas.attr('width', WIDTH).attr('height', HEIGHT);
+};
 
-  stars.forEach(star => {
+windowSize();
 
-    context.beginPath();
-    context.lineCap = 'round';
-    context.lineWidth = STAR_SIZE * star.z * scale;
-    context.strokeStyle = 'rgba(255,255,255,' + (0.5 + 0.5 * Math.random()) + ')';
+$(window).resize(function() {
+windowSize();
+});
 
-    context.beginPath();
-    context.moveTo(star.x, star.y);
+con = canvas[0].getContext('2d');
 
-    var tailX = velocity.x * 2,
-    tailY = velocity.y * 2;
-
-    // stroke() wont work on an invisible line
-    if (Math.abs(tailX) < 0.1) tailX = 0.5;
-    if (Math.abs(tailY) < 0.1) tailY = 0.5;
-
-    context.lineTo(star.x + tailX, star.y + tailY);
-
-    context.stroke();
-
-  });
-
+for (var i = 0; i < 100; i++) {
+pxs[i] = new Circle();
+pxs[i].reset();
 }
 
-function movePointer(x, y) {
-
-  if (typeof pointerX === 'number' && typeof pointerY === 'number') {
-
-    let ox = x - pointerX,
-    oy = y - pointerY;
-
-    velocity.tx = velocity.tx + ox / 8 * scale * (touchInput ? 1 : -1);
-    velocity.ty = velocity.ty + oy / 8 * scale * (touchInput ? 1 : -1);
-
-  }
-
-  pointerX = x;
-  pointerY = y;
-
+requestAnimationFrame(draw);
 }
 
-function onMouseMove(event) {
+function draw () {
+con.clearRect(0, 0, WIDTH, HEIGHT);
+con.globalCompositeOperation = "lighter";
 
-  touchInput = false;
-
-  movePointer(event.clientX, event.clientY);
-
+for (var i = 0; i < pxs.length; i++) {
+pxs[i].fade();
+pxs[i].move();
+pxs[i].draw();
 }
 
-function onTouchMove(event) {
-
-  touchInput = true;
-
-  movePointer(event.touches[0].clientX, event.touches[0].clientY, true);
-
-  event.preventDefault();
-
+requestAnimationFrame(draw);
 }
 
-function onMouseLeave() {
+function Circle() {
+this.s = {
+ttl: 15000,
+xmax: 5,
+ymax: 2,
+rmax: 7,
+rt: 1,
+xdef: 960,
+ydef: 540,
+xdrift: 4,
+ydrift: 4,
+random: true,
+blink: true
+};
 
-  pointerX = null;
-  pointerY = null;
+this.reset = function() {
+this.x = (this.s.random ? WIDTH * Math.random() : this.s.xdef);
+this.y = (this.s.random ? HEIGHT * Math.random() : this.s.ydef);
+this.r = ((this.s.rmax - 1) * Math.random()) + 1;
 
+this.dx = (Math.random() * this.s.xmax) * (Math.random() < 0.5 ? -1 : 1);
+this.dy = (Math.random() * this.s.ymax) * (Math.random() < 0.5 ? -1 : 1);
+
+this.hl = (this.s.ttl / rint) * (this.r / this.s.rmax);
+this.rt = Math.random() * this.hl;
+
+this.stop = Math.random() * 0.2 + 0.4;
+
+this.s.rt = Math.random() + 1;
+this.s.xdrift *= Math.random() * (Math.random() < 0.5 ? -1 : 1);
+this.s.ydrift *= Math.random() * (Math.random() < 0.5 ? -1 : 1);
+};
+
+this.fade = function() {
+this.rt += this.s.rt;
+};
+
+this.draw = function() {
+var newo, cr;
+
+if (this.s.blink && (this.rt <= 0 || this.rt >= this.hl)) {
+  this.s.rt = this.s.rt * -1;
+}
+else if (this.rt >= this.hl) {
+  this.reset();
 }
 
+newo = 1 - (this.rt / this.hl);
 
+con.beginPath();
+con.arc(this.x, this.y, this.r, 0, Math.PI * 2, true);
+con.closePath();
 
-/*console.clear();
-const canvas = document.createElement('canvas');
-document.body.append(canvas);
-canvas.style.display = 'block';
-canvas.style.width = '100%';
-canvas.style.height = '100vh';
+cr = this.r * newo;
 
-canvas.width = canvas.offsetWidth;
-canvas.height = canvas.offsetHeight;
+g = con.createRadialGradient(this.x, this.y, 0, this.x, this.y, (cr <= 0 ? 1 : cr));
+g.addColorStop(0.0, 'rgba(193,254,254,' + newo + ')');
+g.addColorStop(this.stop, 'rgba(193,254,254,' + (newo * 0.2) + ')');
+g.addColorStop(1.0, 'rgba(193,254,254,0)');
 
-const gl = canvas.getContext('webgl2');
-if (!gl) {
-    alert('require webgl 2.0, bye')
-}
+con.fillStyle = g;
+con.fill();
+};
 
-const vss = `#version 300 es
-in vec2 p;
-void main() {
-  gl_Position = vec4(p, 0.0, 1.0);
-}
-`;
+this.move = function() {
+this.x += (this.rt / this.hl) * this.dx;
+this.y += (this.rt / this.hl) * this.dy;
+if (this.x > WIDTH || this.x < 0) this.dx *= -1;
+if (this.y > HEIGHT || this.y < 0) this.dy *= -1;
+};
 
-const fss = `#version 300 es
-precision mediump float;
-out vec4 o;
-uniform vec4 c;
-void main() {
-  o = c;
-}
-`;
+this.getX = function() {
+return this.x;
+};
 
+this.getY = function() {
+return this.y;
+};
+};
+})();
 
-// Create shader program
-// # should query both shader logs and program logs
-// #  only if program link's status is false.
-// 
-// Here's the antipattern .. keep for ref
-//
-
-const vs = gl.createShader(gl.VERTEX_SHADER);
-gl.shaderSource(vs, vss);
-gl.compileShader(vs);
-if (!gl.getShaderParameter(vs, gl.COMPILE_STATUS)) {
-    console.error(gl.getShaderInfoLog(vs));
-    throw 1;
-}
-
-const fs = gl.createShader(gl.FRAGMENT_SHADER);
-gl.shaderSource(fs, fss);
-gl.compileShader(fs);
-if (!gl.getShaderParameter(fs, gl.COMPILE_STATUS)) {
-    console.error(gl.getShaderInfoLog(fs));
-    throw 2;
-}
-
-const prg = gl.createProgram();
-gl.attachShader(prg, vs);
-gl.attachShader(prg, fs);
-gl.linkProgram(prg);
-if (!gl.getProgramParameter(prg, gl.LINK_STATUS)) {
-    console.error(gl.getProgramInfoLog(prg));
-    throw 3;
-}
-
-gl.detachShader(prg, vs);
-gl.deleteShader(vs);
-gl.detachShader(prg, fs);
-gl.deleteShader(fs);
-
-// ---- End of antipattern ----
-
-const $p = gl.getAttribLocation(prg, 'p');
-const $c = gl.getUniformLocation(prg, 'c');
-
-const va = gl.createVertexArray();
-gl.bindVertexArray(va);
-
-const N = 300; // n triangles
-
-let ps;
-{    
-    ps = new Float32Array(2 + N * 2 * 2);
-    ps[0] = 0; // clip space center
-    ps[1] = 0;
-    let j = 2;
-    for (let i = 0; i < N; ++i) {
-        ps[j++] = Math.random() * 2 - 1; //x 
-        ps[j++] = Math.random() * 2 - 1; //y
-        ps[j++] = Math.random() * 2 - 1; //x 
-        ps[j++] = Math.random() * 2 - 1; //y
-    }
-}
-
-const buf = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, buf);
-gl.bufferData(gl.ARRAY_BUFFER, ps, gl.DYNAMIC_DRAW);
-gl.enableVertexAttribArray($p);
-gl.vertexAttribPointer(
-    $p,
-    2, gl.FLOAT, // two 32b-float (8bytes)
-    false,
-    0, // skip n byte to fetch next
-    0  // skip n byte to fetch first
-);
-
-let idxs; 
-{ 
-    idxs = new Uint16Array(3 * N);
-    let j = 0;
-    for (let i = 0; i < N; ++i) {
-        idxs[j++] = 0;
-        idxs[j++] = 1 + i * 2;
-        idxs[j++] = 2 + i * 2;
-    }
-}
-
-const ibuf = gl.createBuffer();
-gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibuf);
-gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, idxs, gl.STATIC_DRAW);
-
-gl.bindVertexArray(null);
-
-//----- render
-
-gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-gl.clearColor(0.1, 0.1, 0.1, 1);
-gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-gl.enable(gl.BLEND);
-gl.disable(gl.CULL_FACE);
-gl.useProgram(prg);
-gl.bindVertexArray(va);
+$('.spriteWrap').sprites();
 
 
 
-function f() {
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.uniform4fv($c, [0.2, 0.2, 0.2, 0.02]);
-    gl.drawElements(
-        gl.TRIANGLES,
-        idxs.length, // n indices
-        gl.UNSIGNED_SHORT, // ui16
-        0 // skip n bytes to fetch first
-    );
-}
-f();
 
-// ---
-gl.bindBuffer(gl.ARRAY_BUFFER, buf);
-document.body.onmousemove = (e) => {
-    ps[0] = e.clientX / window.innerWidth * 2 - 1;
-    ps[1] = -1 * (e.clientY / window.innerHeight * 2 - 1);
-    gl.bufferSubData(gl.ARRAY_BUFFER, 0, ps.slice(0, 2)); // that's why DYNAMIC_DRAW
-    f();
-} */
